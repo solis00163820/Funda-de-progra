67e144d2db313a53029a9a3fb6dabacc464b7168
“solis00163820” <00163820@uca.edu.sv>
2020-05-23 16:04:15 -0600

    Guia 3

---
+++ ejercicio2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float R,D,Perimetro,Area;
+    cout<<"introduce el radio de la circunferencia";
+    cin>>R;
+    Perimetro = 2 * 3.1415169 * R;
+    cout<<"el perimetro de la circunferencia es:"<<Perimetro;
+    Area = (3.1415169 * D * D)/4;
+    cout<<"el area de la circunferencia es:"<<Area;
+    return 0;
+}
